2c95318667f74d656118d14fe0ce51edebcd0370
seun2255 <seunemmanuel2255@gmail.com>
2024-08-25 09:55:39 +0100

    fix: deploy error

--- src/app/other-user-follow/page.tsx
+++ src/app/other-user-follow/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { Suspense } from "react";
 import OtherUserFollow from "@/views/other-user-follow/other-user-follow";
 import React from "react";
 
 export default function OtherUserFollowPage() {
   return (
-    <div>
-      <OtherUserFollow />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        <OtherUserFollow />
+      </div>
+    </Suspense>
   );
 }
